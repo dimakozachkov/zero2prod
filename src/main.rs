1eb8f170d85f2480f7d8a46e7a8515e39b7ccf78
Dmitry Kozachkov <dimakozachkov@gmail.com>
2021-09-16 18:52:38 +0300

    Added a first route and a first integration test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
+
+// #[cfg(test)]
+// mod tests {
+
+//     use crate::health_check;
+
+//     #[actix_rt::test]
+//     async fn health_check_succeeds() {
+//         let response = health_check().await;
+
+//         assert!(response.status().is_success())
+//     }
+
+// }
